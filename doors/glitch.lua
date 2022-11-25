4c9f7f26d016f291865bf643084b1ca35a1c1a57
a <105553346+arabnoob@users.noreply.github.com>
2022-11-25 16:34:29 +0300

    Update glitch.lua

--- doors/glitch.lua
+++ doors/glitch.lua
@@ -1,3 +1,4 @@
      if Key.KeyCode==Enum.KeyCode[getgenv().KeyBind] and not UIS:GetFocusedTextBox() then
           require(game.ReplicatedStorage.ClientModules.EntityModules.Glitch).stuff(require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game),
 workspace.CurrentRooms[game.Players.LocalPlayer:GetAttribute("CurrentRoom")])
+end)
